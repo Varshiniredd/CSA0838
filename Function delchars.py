940c60618077d3d6490767212927558a0501fd4e
Varshiniredd <112492862+Varshiniredd@users.noreply.github.com>
2022-09-15 09:22:11 +0530

    Update Function delchars.py

--- Function delchars.py
+++ Function delchars.py
@@ -1,9 +1,11 @@
-s=str(input("sentence: "))
-r=input("The character to be removed : ")
-sp=""
-for i in (s):
-    if(i==r):
-        continue
-    else :
-        sp=sp+i
-print(sp)
+def charr(s,r):
+    sp=""
+    for i in (s):
+        if(i==r):
+           continue
+        else :
+           sp=sp+i
+    print(  "OUTPUT : ",sp)
+str=str(input("sentence: "))
+r=input(" character to be removed :")
+charr(str,r)
