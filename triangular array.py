98d7aef7d63473f343a7db94a7fa1c5f866fbe3b
Varshiniredd <112492862+Varshiniredd@users.noreply.github.com>
2022-09-16 09:23:39 +0530

    Update triangular array.py

--- triangular array.py
+++ triangular array.py
@@ -2,12 +2,12 @@ r=int(input("enter the range:"))
 s=int(input("enter the row to find its sum:"))
 if(s<=0 or r<=0):
     print("wrong input")
-elif(s<=r):
+elif(s>r):
     print("wrong input")
 elif(s<= r):
     t=0
     for i in range (1,r+1):
-        for i in range (0,r-i+1):
+        for j in range (0,r-i+1):
             print(" ",end =' ')
         c=1
         for j in range( 1,i+1):
